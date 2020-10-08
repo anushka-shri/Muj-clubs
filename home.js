5606923e365e9ca46d0b7d4919636597b51a55a8
Anushka Shrivastava <ishasxd@gmail.com>
2020-10-08 22:38:11 +0530

    up button

---
+++ home.js
@@ -0,0 +1,21 @@
+
+upInteractive = false;
+
+function autoToggle() {
+    $('.arrow').toggleClass('auto');
+}
+
+$('.arrow').hover(function () {
+    upInteractive = true;
+    $('.arrow').removeClass('auto');
+});
+
+setInterval(function () {
+
+    console.log(upInteractive);
+
+    if (upInteractive === false) {
+        autoToggle();
+    }
+
+}, 2000);
